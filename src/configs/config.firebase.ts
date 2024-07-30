e4fe9749c424a3f969026e6ffe759d18fbfb59b9
baophung020394 <bao.future94@gmail.com>
2024-07-30 18:51:31 +0700

    Update env file

--- src/configs/config.firebase.ts
+++ src/configs/config.firebase.ts
@@ -2,15 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import * as admin from "firebase-admin";
 import serviceAccount from "./serviceaccount.json";
+import * as dotenv from "dotenv";
+
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.production" });
+} else {
+  dotenv.config({ path: ".env" });
+}
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.BACKEND_FIREBASE_API_KEY,
+  authDomain: process.env.BACKEND_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.BACKEND_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.BACKEND_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.BACKEND_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.BACKEND_FIREBASE_APP_ID,
+  measurementId: process.env.BACKEND_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
